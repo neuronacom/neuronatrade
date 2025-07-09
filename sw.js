3b811a2349734525e96d3268624ea5822bd29d17
Neurona 1.0 <neuronapost@gmail.com>
2025-07-10 00:16:46 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-self.addEventListener("install", e => self.skipWaiting());
-self.addEventListener("activate", e => self.clients.claim());
-self.addEventListener("fetch", e => {});
+self.addEventListener('install', e => {self.skipWaiting();});
+self.addEventListener('activate', e => {self.clients.claim();});
+self.addEventListener('fetch', () => {});
