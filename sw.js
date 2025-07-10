c0fb45e37192238fa4fb0db6857e33eaa7640141
Neurona 1.0 <neuronapost@gmail.com>
2025-07-10 22:00:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-self.addEventListener("install",e=>{self.skipWaiting();});
-self.addEventListener("activate",e=>{self.clients.claim();});
-self.addEventListener("fetch",()=>{});
+const CACHE_NAME = 'neurona-trade-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
