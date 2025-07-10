6c00560a7628c03deb452eae2fcb2bedd5c8d237
Neurona 1.0 <neuronapost@gmail.com>
2025-07-10 20:56:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,10 @@
-self.addEventListener('install', e=>self.skipWaiting());
-self.addEventListener('activate', e=>self.clients.claim());
-self.addEventListener('fetch', e=>{
-  if(e.request.method==='GET')
-    e.respondWith(caches.open('v1').then(cache=>
-      cache.match(e.request).then(resp=>resp||fetch(e.request).then(r=>{
-        cache.put(e.request,r.clone()); return r;
-      }))
-    ));
-});
-self.addEventListener('notificationclick', e=>{
-  if(e.notification.data && e.notification.data.url)
-    clients.openWindow(e.notification.data.url);
-  e.notification.close();
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+
+self.addEventListener('notificationclick', function(event) {
+  let url = event.notification.data && event.notification.data.url;
+  if (url) {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(url));
+  }
 });
